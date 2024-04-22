d8f5890b917e9e7514473d98a56473252cbb978e
Jaures Tchoffo <tchoffojaures60@gmail.com>
2024-04-22 10:39:16 +0200

    entity created

---
+++ src/Entity/Contact.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $phoneNumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(int $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Message.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contentMessage = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $scheduleDate = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $scheduleTime = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateMessage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContentMessage(): ?string
+    {
+        return $this->contentMessage;
+    }
+
+    public function setContentMessage(string $contentMessage): static
+    {
+        $this->contentMessage = $contentMessage;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getScheduleDate(): ?\DateTimeInterface
+    {
+        return $this->scheduleDate;
+    }
+
+    public function setScheduleDate(?\DateTimeInterface $scheduleDate): static
+    {
+        $this->scheduleDate = $scheduleDate;
+
+        return $this;
+    }
+
+    public function getScheduleTime(): ?\DateTimeInterface
+    {
+        return $this->scheduleTime;
+    }
+
+    public function setScheduleTime(?\DateTimeInterface $scheduleTime): static
+    {
+        $this->scheduleTime = $scheduleTime;
+
+        return $this;
+    }
+
+    public function getDateMessage(): ?\DateTimeInterface
+    {
+        return $this->dateMessage;
+    }
+
+    public function setDateMessage(\DateTimeInterface $dateMessage): static
+    {
+        $this->dateMessage = $dateMessage;
+
+        return $this;
+    }
+}
