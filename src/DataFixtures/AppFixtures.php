3c9e457e261504e6c7bd4e90de08cd0bf5930193
Jaures Tchoffo <tchoffojaures60@gmail.com>
2024-04-24 09:35:43 +0200

    message add on db

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Message;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -35,6 +36,21 @@ class AppFixtures extends Fixture
         $manager->persist($userAdmin);
         // $product = new Product();
         // $manager->persist($product);
+         // Création des messages
+         $message1 = new Message();
+         $message1->setContentMessage("Bonjour, ceci est un message de test.");
+         $message1->setStatus("sent");
+         $message1->setDateMessage(new \DateTime('now'));
+         $message1->setSender($user); // Associé à l'utilisateur normal
+         $manager->persist($message1);
+ 
+         $message2 = new Message();
+         $message2->setContentMessage("Ceci est un autre message de test.");
+         $message2->setStatus("draft");
+         $message2->setDateMessage(new \DateTime('now'));
+         $message2->setSender($userAdmin); // Associé à l'administrateur
+         $manager->persist($message2);
+ 
 
         $manager->flush();
     }
