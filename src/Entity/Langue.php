9d05e0a2d8ed5067ee2fc15756570dbcbcb5cda8
Jaures Tchoffo <tchoffojaures60@gmail.com>
2024-04-25 13:26:56 +0200

    update

--- src/Entity/Langue.php
+++ src/Entity/Langue.php
@@ -2,17 +2,24 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\LangueRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    normalizationContext: ['groups' => ['read'], 'enable_max_depth' => true],
+    denormalizationContext: ['groups' => ['write']]
+)]
 #[ORM\Entity(repositoryClass: LangueRepository::class)]
 class Langue
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(["read", "write"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
