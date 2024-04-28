4e9ee28523a2a5bfab3c6d81e4d4ec8532e3d979
Jaures Tchoffo <tchoffojaures60@gmail.com>
2024-04-28 08:00:28 +0200

    update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -22,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    private ?string $email = null;
+    private ?string $email = null; 
 
     /**
      * @var list<string> The user roles
